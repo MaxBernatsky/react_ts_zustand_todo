a04dbe59a37521ab63c8d116b34c2cc6b3a0bc30
Maxim Bernatsky <maxsateon@yandex.ru>
2023-07-10 23:52:23 +0300

    feat: create todo store

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./view/App/App.tsx";
 
+import "./view/styles/reset.scss";
+import "./view/styles/common.scss";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- src/view/App/App.tsx
+++ src/view/App/App.tsx
@@ -1,5 +1,17 @@
 import { FC } from "react";
+import styles from "./App.module.scss";
+import { useToDoStore } from "../../data/stores/useToDoStore.ts";
 
 export const App: FC = () => {
-  return <div>hello</div>;
+  const { tasks, createTask, removeTask, updateTask } = useToDoStore(
+    (state) => state,
+  );
+  console.log(tasks, createTask, removeTask, updateTask);
+  return (
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>To Do App</h1>
+      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}></section>
+    </article>
+  );
 };
